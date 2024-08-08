eecc36264e3b5944aac150c5c04baf23f3a5554b
ken Nguyen <hoanglong020597@gmail.com>
2024-08-08 10:59:54 +0700

    ud

--- begin.sh
+++ begin.sh
@@ -5,6 +5,23 @@ cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/
 git clone https://github.com/KentNguyen90/ccminer.git
 cd ccminer
 chmod +x build.sh configure.sh autogen.sh start.sh
+
+# Thêm phần nhập liệu
+echo "Nhập address wallet:"
+read wallet
+echo "Nhập pool 'stratum+tcp://...' (để trống để sử dụng pool mặc định của VIPOR):"
+read pool
+if [ -z "$pool" ]; then
+  pool="stratum+tcp://cn.vipor.net:5040"
+fi
+echo "Nhập tên máy:"
+read name
+
+# Cập nhật config.json
+sed -i "s|RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa|$wallet|g" config.json
+sed -i "s|stratum+tcp://cn.vipor.net:5040|$pool|g" config.json
+sed -i "s|PHONE-xx|$name|g" config.json
+
 if [ ! -f ~/.bashrc ]; then
   echo "~/ccminer/start.sh" > ~/.bashrc
 else
@@ -12,7 +29,9 @@ else
     echo "~/ccminer/start.sh" >> ~/.bashrc
   fi
 fi
+
 CXX=clang++ CC=clang ./build.sh
-echo "setup nearly complete."
-echo "Edit the config with \"nano ~/ccminer/config.json\""
-echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
+
+echo "Thiết lập gần hoàn tất."
+echo "Để cấu hình lại, nhập lệnh \"nano ~/ccminer/config.json\""
+echo "Sau khi cài đặt xong khởi động lại máy để đào ổn định hơn."
