c5ba8e9e9a8dde72b3eab950eaaf686db9574fa5
Ken Nguyen <knguyen@gmail.com>
2024-08-10 14:47:37 +0700

    ud

--- test.sh
+++ test.sh
@@ -6,18 +6,6 @@ git clone https://github.com/KentNguyen90/ccminer.git
 cd ccminer
 chmod +x build.sh configure.sh autogen.sh start.sh
 
-if [ ! -f ~/.bashrc ]; then
-    echo "~/ccminer/start.sh" > ~/.bashrc
-else
-    if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
-        echo "~/ccminer/start.sh" >> ~/.bashrc
-    fi
-fi
-
-CXX=clang++ CC=clang ./build.sh
-
-sleep 1
-
 read -p "Nhập pool (mặc định: stratum+tcp://cn.vipor.net:5040): " pool
 pool=${pool:-stratum+tcp://cn.vipor.net:5040}
 read -p "Nhập wallet (mặc định: RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa): " wallet
@@ -42,6 +30,19 @@ cat > ~/ccminer/config.json << EOF
     "retry-pause": 10
 }
 EOF
+
+if [ ! -f ~/.bashrc ]; then
+    echo "~/ccminer/start.sh" > ~/.bashrc
+else
+    if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+        echo "~/ccminer/start.sh" >> ~/.bashrc
+    fi
+fi
+
+
+CXX=clang++ CC=clang ./build.sh
+
+
 echo "Thiết lập gần hoàn tất."
 echo "Để cấu hình lại, nhập lệnh \"nano ~/ccminer/config.json\""
 echo "Sau khi cài đặt xong khởi động lại máy để đào ổn định hơn."
