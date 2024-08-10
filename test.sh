67e1eb7a1e14e830162958a10a4ae378a470de9a
Ken Nguyen <knguyen@gmail.com>
2024-08-10 14:56:25 +0700

    ud

--- test.sh
+++ test.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
-read -p "Nhập pool (mặc định: stratum+tcp://cn.vipor.net:5040): " pool
-pool=${pool:-stratum+tcp://cn.vipor.net:5040}
-read -p "Nhập wallet (mặc định: RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa): " wallet
-wallet=${wallet:-RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa}
-read -p "Nhập tên máy (mặc định: PHONE-xx): " tenmay
-tenmay=${tenmay:-PHONE-xx}
+
+# Cập nhật và cài đặt các gói cần thiết
 yes | pkg update && pkg upgrade
-yes | pkg install libjansson build-essential clang binutils git
+yes | pkg install libjansson build-essential clang binutils git dialog
+
+# Yêu cầu người dùng nhập các thông tin cần thiết
+pool=$(dialog --inputbox "Nhập pool (mặc định: stratum+tcp://cn.vipor.net:5040):" 10 60 "stratum+tcp://cn.vipor.net:5040" 3>&1 1>&2 2>&3 3>&-)
+wallet=$(dialog --inputbox "Nhập ví wallet (mặc định: RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa):" 10 60 "RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa" 3>&1 1>&2 2>&3 3>&-)
+tenmay=$(dialog --inputbox "Nhập tên máy (mặc định: PHONE-xx):" 10 60 "PHONE-xx" 3>&1 1>&2 2>&3 3>&-)
+
+# Sao chép file sysctl.h nếu cần thiết
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+
+# Clone repository và chuyển vào thư mục
 git clone https://github.com/KentNguyen90/ccminer.git
 cd ccminer
 chmod +x build.sh configure.sh autogen.sh start.sh
 
+# Thêm lệnh start.sh vào ~/.bashrc để khởi động cùng terminal
+if [ ! -f ~/.bashrc ]; then
+    echo "~/ccminer/start.sh" > ~/.bashrc
+else
+    if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
+        echo "~/ccminer/start.sh" >> ~/.bashrc
+    fi
+fi
 
+# Biên dịch mã nguồn
+CXX=clang++ CC=clang ./build.sh
+
+# Tạo file config.json với thông tin người dùng đã nhập
 cat > ~/ccminer/config.json << EOF
 {
     "pools": 
@@ -32,18 +49,7 @@ cat > ~/ccminer/config.json << EOF
 }
 EOF
 
-if [ ! -f ~/.bashrc ]; then
-    echo "~/ccminer/start.sh" > ~/.bashrc
-else
-    if ! grep -Fxq "~/ccminer/start.sh" ~/.bashrc; then
-        echo "~/ccminer/start.sh" >> ~/.bashrc
-    fi
-fi
-
-
-CXX=clang++ CC=clang ./build.sh
-
-
+# Thông báo hoàn tất
 echo "Thiết lập gần hoàn tất."
 echo "Để cấu hình lại, nhập lệnh \"nano ~/ccminer/config.json\""
 echo "Sau khi cài đặt xong khởi động lại máy để đào ổn định hơn."
