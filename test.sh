179d0ddb1fb725fe0dfc11d45a14381e05e56a4a
Ken Nguyen <knguyen@gmail.com>
2024-08-10 16:16:38 +0700

    ud

--- test.sh
+++ test.sh
@@ -4,10 +4,6 @@
 yes | pkg update && pkg upgrade
 yes | pkg install libjansson build-essential clang binutils git dialog
 
-# Yêu cầu người dùng nhập các thông tin cần thiết
-pool=$(dialog --inputbox "Nhập pool (mặc định: stratum+tcp://cn.vipor.net:5040):" 10 60 "stratum+tcp://cn.vipor.net:5040" 3>&1 1>&2 2>&3 3>&-)
-wallet=$(dialog --inputbox "Nhập ví wallet (mặc định: RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa):" 10 60 "RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa" 3>&1 1>&2 2>&3 3>&-)
-tenmay=$(dialog --inputbox "Nhập tên máy (mặc định: PHONE-xx):" 10 60 "PHONE-xx" 3>&1 1>&2 2>&3 3>&-)
 
 # Sao chép file sysctl.h nếu cần thiết
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
@@ -28,6 +24,10 @@ fi
 
 # Biên dịch mã nguồn
 CXX=clang++ CC=clang ./build.sh
+# Yêu cầu người dùng nhập các thông tin cần thiết
+pool=$(dialog --inputbox "Nhập pool (mặc định: stratum+tcp://cn.vipor.net:5040):" 10 60 "stratum+tcp://cn.vipor.net:5040" 3>&1 1>&2 2>&3 3>&-)
+wallet=$(dialog --inputbox "Nhập ví wallet (mặc định: RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa):" 10 60 "RRssVi5MDs5MUAkbtBWbCTfcRy8qbua4Fa" 3>&1 1>&2 2>&3 3>&-)
+tenmay=$(dialog --inputbox "Nhập tên máy (mặc định: PHONE-xx):" 10 60 "PHONE-xx" 3>&1 1>&2 2>&3 3>&-)
 
 # Tạo file config.json với thông tin người dùng đã nhập
 cat > ~/ccminer/config.json << EOF
